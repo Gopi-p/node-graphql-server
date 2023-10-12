0799caf3ab53defb83073e3e88e4eb5fd9878bf3
Gopinath P <gopinath.p@tandemloop.in>
2023-10-12 14:48:33 +0530

    config updated and basic server added

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Express, Request, Response } from 'express';
+
+
+const app: Express = express();
+const port = 3001;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.get('/hello', (req: Request, res: Response) => {
+  res.send('Hello From server');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
